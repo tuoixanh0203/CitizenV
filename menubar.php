b0ba753034105c4cce045b3a89c054a321bfe879
tuoixanh0203 <62226586+tuoixanh0203@users.noreply.github.com>
2021-12-16 22:38:24 +0700

    .

--- menubar.php
+++ menubar.php
@@ -4,31 +4,27 @@
       <!-- Sidebar user panel -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">REPORTS</li>
-        <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-        <li class="header">MANAGE</li>
+        <li class="header">BÁO CÁO</li>
+        <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>Trang chủ</span></a></li>
+        <li class="header">QUẢN LÝ</li>
         
-        <li><a href=""><i class="fa fa-calendar"></i> <span>Attendance</span></a></li>
+        
         <li class="treeview">
           <a href="#">
             <i class="fa fa-users"></i>
-            <span>Employees</span>
+            <span>Chi cục dân số</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href=""><i class="fa fa-circle-o"></i> Employee List</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Overtime</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Cash Advance</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Schedules</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Cấp mã tỉnh</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Cấp tài khoản</a></li>
           </ul>
         </li>
-        <li><a href=""><i class="fa fa-file-text"></i> Deductions</a></li>
-        <li><a href=""><i class="fa fa-suitcase"></i> Positions</a></li>
-        <li class="">PRINTABLES</li>
-        <li><a href=""><i class="fa fa-files-o"></i> <span>Payroll</span></a></li>
-        <li><a href=""><i class="fa fa-clock-o"></i> <span>Schedule</span></a></li>
+        <li><a href=""><i class="fa fa-calendar"></i> <span>Công dân</span></a></li>
+        <li class="header">BẢN IN</li>
+        <li><a href=""><i class="fa fa-files-o"></i> <span>Danh sách công dân</span></a></li>
       </ul>
     </section>
     <!-- /.sidebar -->
--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@ session_start();
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b></b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b></b>HRM System</span>
+      <span class="logo-lg"><b></b>CitizenV</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
