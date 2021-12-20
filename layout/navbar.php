40bb31b0ed5d1d5bf11ca62375289bad1ee9a854
ly3012 <71602612+ly3012@users.noreply.github.com>
2021-12-20 23:55:45 +0700

    style

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,18 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-auto ms-lg-0 ms-3 mt-2 text-uppercase fw-bold bg-white text-success" href="home.php">citizenv</a>
+    <a class="navbar-brand me-auto ms-lg-0 ms-3 mt-2 text-uppercase fw-bold" href="home.php">citizenv</a>
 
     <div class="collapse navbar-collapse" id="topNavBar">
-      <form class="d-flex ms-auto my-3 my-lg-0">
-        <div class="input-group">
-          <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-primary" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </form>
-
-      <li class="navbar-nav nav-item dropdown">
+      <li class="navbar-nav nav-item dropdown d-flex ms-auto my-3 my-lg-0">
         <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <i class="fas fa-user"></i>
         </a>
