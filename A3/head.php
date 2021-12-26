595b04dcb3a87816ef5ffd85a1e163edc4314076
tuoixanh0203 <62226586+tuoixanh0203@users.noreply.github.com>
2021-12-26 16:08:00 +0700

    logo

--- A3/head.php
+++ A3/head.php
@@ -11,7 +11,7 @@
   <!-- <link rel="stylesheet" href="dataTables.bootstrap5.min.css" /> -->
   <link rel="stylesheet" href="css/layout/style.css" />
   <link rel="stylesheet" href="css/login.css">
-  <link rel='shortcut icon' href='https://population.edu.vn/wp-content/uploads/2020/10/logo285.jpg' />
+  <link rel='shortcut icon' href='http://gopfp.gov.vn/CTT_QLTT-theme/images/favicon.ico' />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
